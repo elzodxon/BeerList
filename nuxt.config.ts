3d99b487894ec67c96181ef2bb519247871541ce
Elzodxon Sharofaddinov <elzodxon@gmail.com>
2023-06-13 22:53:13 +0500

    feat: meta title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: true,
+  app: {
+    head:{
+      title: 'Beers website'
+    }
+  },
   css: ['@/assets/styles/main.css'],
   modules: [
     '@nuxtjs/tailwindcss',

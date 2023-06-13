545594fa84a062150efd78a20364c686ac318890
Elzodxon Sharofaddinov <elzodxon@gmail.com>
2023-06-13 22:32:18 +0500

    chore: init project architechture and main logic finished

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container mx-auto">
+    <LayoutHeader class="my-5"/>
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style>
+.container {
+  padding: 2rem;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <BeerList v-bind="{beers}" :items-per-page="10" />
+</template>
+
+<script setup lang="ts">
+
+import {useApi} from "~/composables/useApi";
+
+const { data: beers, error, refresh } = await useAsyncData(
+    'beers',
+    () => useApi().get('/v2/beers', {
+      params:{
+          brewed_after: '11-2012'
+      }
+    })
+)
+
+</script>
